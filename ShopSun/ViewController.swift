6d0492b817e169e48dd164663fcb38a1b9db8dbc
sun-23 <twinsamson@gmail.com>
2019-04-23 11:45:34 +0700

    UI register OK

--- ShopSun/ViewController.swift
+++ ShopSun/ViewController.swift
@@ -9,11 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var userTextField: UITextField!
+    
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func loginButton(_ sender: Any) {
+        print("You Click Login")
+    }
+    
+    
+    
+    @IBAction func registerButton(_ sender: UIButton) {
+        print("You Click Register")
+    }
+    
 
 
 }
