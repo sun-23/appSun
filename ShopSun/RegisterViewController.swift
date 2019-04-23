c6369246685b93848ba1f9a9d7e0056f8e43f488
sun-23 <twinsamson@gmail.com>
2019-04-23 16:32:44 +0700

    register ok

--- ShopSun/RegisterViewController.swift
+++ ShopSun/RegisterViewController.swift
@@ -43,7 +43,7 @@ class RegisterViewController: UIViewController {
         let user = userTextField.text!
         let password = passwordTextField.text!
         
-        print("name ==>> \(name)")
+        print("name ==>> \(name)") 
         print("user ==>> \(user)")
         print("password ==>> \(password)")
         
